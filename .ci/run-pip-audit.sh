c945cf3dca9ebc1b7d1c3a35ce3d3c0be62f7bfe
imda-kwokwk <kwok_weng_kong@imda.gov.sg>
2024-08-07 19:37:46 +0800

    add pre-build-checks workflow

--- .ci/run-pip-audit.sh
+++ .ci/run-pip-audit.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-pip uninstall -r requirements.txt -y
-
-# create venv for ci
-python3 -m venv ci-venv
-source ci-venv/bin/activate
-
-# install dependencies
-pip install -r requirements.txt
+pip uninstall pytest pytest-mock pytest-html pytest-json pytest-cov coverage httpx anybadge -y
+pip uninstall flake8 flake8-html -y
 
 # license check
 pip install pip-licenses
@@ -26,43 +20,11 @@ mdtree licenses-found.md > license-report.html
 
 # Create badges
 pip install anybadge
-python3 ci/createBadges.py dependency
-python3 ci/createBadges.py license
-
-deactivate
-rm -rf ci-venv
+python3 .ci/createBadges.py dependency
+python3 .ci/createBadges.py license
 
 set -e
 if [ $exit_code -ne 0 ]; then
   echo "pip-audit failed, exiting..."
   exit $exit_code
 fi
-
-#pip uninstall pytest pytest-mock pytest-html pytest-json pytest-cov coverage httpx anybadge -y
-#pip uninstall flake8 flake8-html -y
-#
-## license check
-#pip install pip-licenses
-#pip-licenses --format markdown --output-file licenses-found.md
-#pip uninstall pip-licenses prettytable wcwidth -y
-#
-## dependency check
-#pip install pip-audit
-#pip uninstall setuptools -y
-#set +e
-#pip-audit --format markdown --desc on -o pip-audit-report.md &> pip-audit-count.txt
-#exit_code=$?
-#pip install mdtree
-#mdtree pip-audit-report.md > pip-audit-report.html
-#mdtree licenses-found.md > license-report.html
-#
-## Create badges
-#pip install anybadge
-#python3 .ci/createBadges.py dependency
-#python3 .ci/createBadges.py license
-#
-#set -e
-#if [ $exit_code -ne 0 ]; then
-#  echo "pip-audit failed, exiting..."
-#  exit $exit_code
-#fi
