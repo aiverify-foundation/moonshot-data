e2cf4611c4dd478a81e10e9134ba93fbc9ed8218
imda-jacksonboey <jackson_boey@imda.gov.sg>
2025-05-22 10:44:58 +0800

    Update run-flake8.sh

--- .ci/run-flake8.sh
+++ .ci/run-flake8.sh
@@ -12,10 +12,10 @@ done
 
 
 set +e
-flake8 --format=html --htmldir=flake8-report --count  $src_dirs > flake8-report.txt
+flake8 --format=html --htmldir=flake8-report --show-source --count  $src_dirs > flake8-report.txt
 cat flake8-report.txt
 exit_code=$?
-#python3 .ci/createBadges.py lint
+python3 .ci/createBadges.py lint
 
 if [ $exit_code -ne 0 ]; then
   echo "flake8 failed, exiting..."
