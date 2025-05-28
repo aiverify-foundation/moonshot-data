8b58a3ee17953d1373696bba0cb5b1e1fd696459
kwk <kwkinet@gmail.com>
2025-05-28 20:31:50 +0800

    cleanup workflows

--- .ci/run-flake8.sh
+++ .ci/run-flake8.sh
@@ -10,14 +10,11 @@ for src_dir in "${src_dir_list[@]}"; do
   src_dirs+="$src_dir "
 done
 
-
 set +e
 flake8 --format=html --htmldir=flake8-report --count  $src_dirs > flake8-report.txt
 cat flake8-report.txt
 exit_code=$?
-# python3 .ci/createBadges.py lint
 
 if [ $exit_code -ne 0 ]; then
-  echo "flake8 failed, exiting..."
   exit $exit_code
 fi
